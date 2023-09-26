a735625b8d3927da70324292e33bf18f3cf2263b
Naveen Malik <96684664+naveenmalik28@users.noreply.github.com>
2023-09-26 23:54:50 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,112 +1,65 @@
-<?php
+<?php 
 
-require_once ("./component.php");
-require_once ("./operation.php");
+include 'config.php';
+
+
+session_start();
+
+error_reporting(0);
+
+if (isset($_SESSION['username'])) {
+    header("Location: welcome.php");
+}
+
+if (isset($_POST['submit'])) {
+	$email = $_POST['email'];
+	$password = md5($_POST['password']);
+
+	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+	$result = mysqli_query($conn, $sql);
+	if ($result->num_rows > 0) {
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['username'] = $row['username'];
+		header("Location: welcome.php");
+	} else {
+		echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
+	}
+}
+
 ?>
 
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pulsus Crud Operation</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <!-- Custom stylesheet -->
-    <link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 
+	<title>Login Form - Assesment Test_Pulsus Healthtech LLP</title>
 </head>
 <body>
 
-<main>
-    <div class="container text-center">
-        <h1 class="py-4 bg-black text-light rounded tital"></i> Pulsus Crud Operation</h1>
-        <div class="d-flex justify-content-center">
-            <form action="" method="post" class="w-50">
-                <div class="pt-2">
-                    <?php inputElement("<i class='fas fa-id-badge'></i>","ID", "book_id",setID()); ?>
-                </div>
-
-                <div class="pt-2">
-                    <?php inputElement("<i class='fas fa-book'></i>","Name", "name",""); ?>
-                </div>
-
-                <div class="row pt-2">
-                    <div class="col">
-                        <?php inputElement("<i class='fas fa-people-carry'></i>","Address", "address",""); ?>
-                    </div>
-
-                    <div class="col">
-                        <?php inputElement("<i class='fas fa-dollar-sign'></i>","Product", "product",""); ?>
-                    </div>
-                </div>
-
-                <div class="d-flex justify-content-center">
-                        <?php buttonElement("btn-create","btn btn-success","Create</i>","create","data-toggle='tooltip' data-placement='bottom' title='Create'"); ?>
-                        <?php buttonElement("btn-read","btn btn-primary","<i class='fas fa-sync'> Add</i>","read","data-toggle='tooltip' data-placement='bottom' title='Read'"); ?>
-                        <?php buttonElement("btn-update","btn btn-light border","<i class='fas fa-pen-alt'> Update</i>","update","data-toggle='tooltip' data-placement='bottom' title='Update'"); ?>
-                        <?php buttonElement("btn-delete","btn btn-danger","<i class='fas fa-trash-alt'>Delete</i>","delete","data-toggle='tooltip' data-placement='bottom' title='Delete'"); ?>
-                        <?php deleteBtn();?>
-                </div>
-            </form>
-        </div>
-
-        <!-- Bootstrap table  -->
-        <div class="d-flex table-data">
-            <table class="table table-striped table-dark">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Address</th>
-                        <th>Product</th>
-                        <th>Edit</th>
-                    </tr>
-                </thead>
-                <tbody id="tbody">
-                   <?php
+	<div class="container">
+		
+		<form action="" method="POST" class="login-email">
+			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Login_Pulsus</p>
+			<div class="input-group">
+				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+			</div>
+			<div class="input-group">
+				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+			</div>
+			<div class="input-group">
+				<button name="submit" class="btn">Login</button>
+			</div>
+			<p class="login-register-text">Don't have an account? <a href="register.php">Register Here</a>.</p>
+		</form>
+	</div>
+</div>
 
 
-                   if(isset($_POST['read'])){
-                       $result = getData();
-
-                       if($result){
-
-                           while ($row = mysqli_fetch_assoc($result)){ ?>
-
-                               <tr>
-                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['id']; ?></td>
-                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['name']; ?></td>
-                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['address']; ?></td>
-                                   <td data-id="<?php echo $row['id']; ?>"><?php echo '' . $row['product']; ?></td>
-                                   <td ><i class="fas fa-edit btnedit" data-id="<?php echo $row['id']; ?>"></i></td>
-                               </tr>
-
-                   <?php
-                           }
-
-                       }
-                   }
-
-
-                   ?>
-                </tbody>
-            </table>
-        </div>
-
-
-    </div>
-</main>
-
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-<script src="../crud/php/main.js"></script>
 </body>
 </html>
